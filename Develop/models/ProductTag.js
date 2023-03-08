4546b6473acc8f48a13718173550555780a0d82f
Jailene <jaileneveras8@gmail.com>
2023-03-08 16:26:24 -0500

    defined columns in model folder

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -5,7 +5,29 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
+  {id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id',
+      unique: false
+    }
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Tag',
+      key: 'id',
+      unique: false
+    }
+  },
+
     // define columns
   },
   {
